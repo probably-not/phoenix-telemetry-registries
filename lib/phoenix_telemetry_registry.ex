f83647c748b11a07e653be10b11f1c3c96cf2398
Coby Benveniste <coby.benveniste@gmail.com>
2024-03-13 12:06:52 +0200

    Initialize repository

---
+++ lib/phoenix_telemetry_registry.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixTelemetryRegistry do
+  @moduledoc """
+  Documentation for `PhoenixTelemetryRegistry`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixTelemetryRegistry.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_telemetry_registry_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixTelemetryRegistryTest do
+  use ExUnit.Case
+  doctest PhoenixTelemetryRegistry
+
+  test "greets the world" do
+    assert PhoenixTelemetryRegistry.hello() == :world
+  end
+end
